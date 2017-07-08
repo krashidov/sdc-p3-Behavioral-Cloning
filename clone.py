587e2f562d957b5cb8d6b1776da502af356b1bf3
krashidov <komran.rashidov@gmail.com>
2017-07-08 01:47:33 -0600

    nvidia works now too

--- clone.py
+++ clone.py
@@ -36,7 +36,7 @@ def generator(samples, batch_size=32):
                 left_image = cv2.imread(left_name)
                 right_image = cv2.imread(right_name)
                 
-                correction = 0.21
+                correction = 0.25
                 center_angle = float(batch_sample[3])
                 left_angle =  center_angle + correction
                 right_angle = center_angle - correction
@@ -74,19 +74,8 @@ def generator(samples, batch_size=32):
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
-ch, row, col = 3, 80, 320  # Trimmed image format
-
-
-nb_filters1 = 16
-nb_filters2 = 8
-nb_filters3 = 4
-nb_filters4 = 2
-
-# size of pooling area for max pooling
-pool_size = (2, 2)
-
 # convolution kernel size
-kernel_size = (3, 3)
+kernel_size = 3
 
 nb_train = len(train_samples) * 6
 nb_valid = len(validation_samples) * 6
@@ -94,51 +83,59 @@ nb_valid = len(validation_samples) * 6
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation 
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+
+#Crop out the useless bits
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
 
+
+###### NVIDIA Self driving architecture
+### Code based on https://github.com/andrewraharjo/SDCND_Behavioral_Cloning/blob/master/model.py
 # Starting with the convolutional layer
 # The first layer will turn 1 channel into 16 channels
-model.add(Convolution2D(nb_filters1, kernel_size[0], kernel_size[1], border_mode='valid', batch_input_shape=(80, 320, 16, 3)))
-# Applying ReLU
+model.add(Convolution2D(3, 5, 5, border_mode='valid', subsample=(2,2), batch_input_shape=(90, 320, 16, 3)))
 model.add(Activation('relu'))
+
 # The second conv layer will convert 16 channels into 8 channels
-model.add(Convolution2D(nb_filters2, kernel_size[0], kernel_size[1]))
-# Applying ReLU
+model.add(Convolution2D(24, 5, 5, subsample=(2,2)))
 model.add(Activation('relu'))
+
 # The second conv layer will convert 8 channels into 4 channels
-model.add(Convolution2D(nb_filters3, kernel_size[0], kernel_size[1]))
-# Applying ReLU
+model.add(Convolution2D(36, 5, 5, subsample=(2,2)))
 model.add(Activation('relu'))
+
 # The second conv layer will convert 4 channels into 2 channels
-model.add(Convolution2D(nb_filters4, kernel_size[0], kernel_size[1]))
-# Applying ReLU
+model.add(Convolution2D(48, 3, 3))
 model.add(Activation('relu'))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+
 # Apply Max Pooling for each 2 x 2 pixels
-model.add(MaxPooling2D(pool_size=pool_size))
-# Apply dropout of 25%
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+#Apply dropout of 50%
 model.add(Dropout(0.25))
 
-# Flatten the matrix. The input has size of 360
 model.add(Flatten())
-# Input 360 Output 16
-model.add(Dense(16))
-# Applying ReLU
+model.add(Dense(100))
 model.add(Activation('relu'))
+
 # Input 16 Output 16
-model.add(Dense(16))
-# Applying ReLU
+model.add(Dense(50))
 model.add(Activation('relu'))
+
 # Input 16 Output 16
-model.add(Dense(16))
-# Applying ReLU
+model.add(Dense(10))
 model.add(Activation('relu'))
+
 # Apply dropout of 50%
 model.add(Dropout(0.5))
-# Input 16 Output 1
+# Input 16 Output steering angle
 model.add(Dense(1))
+
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-model.fit_generator(train_generator, samples_per_epoch=nb_train, validation_data=validation_generator, nb_val_samples=nb_valid, nb_epoch=3)
-model.save('./model.h5')
+model.fit_generator(train_generator, samples_per_epoch=nb_train, validation_data=validation_generator, nb_val_samples=nb_valid, nb_epoch=5)
+model.save('./model-2.h5')
 import gc
 gc.collect()
 
